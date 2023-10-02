cdaed40ce2c47d2030fadc175e4772b063668854
LEEROY KIPLAGAT <leeroykiplagat@gmail.com>
2023-10-02 11:55:19 +0300

    Final Code

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,30 @@
+import PageNotFoundImg from "../assets/images/pagenotfound.png";
+import { Link } from "react-router-dom";
+import { useTitle } from "../hooks/useTitle";
+
+export const PageNotFound = () => {
+  useTitle(`Page Not Found/Cinemate`);
+
+  return (
+    <main>
+      <section className="flex flex-col justify-center px-2">
+        <div className="flex flex-col items-center my-4">
+          <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white">
+            {" "}
+            404, oops!
+          </p>
+          <div>
+            <img className="rounded h-96" src={PageNotFoundImg} alt="" />
+          </div>
+        </div>
+        <div className="flex justify-center my-4">
+          <Link to="/">
+            <button className="bg-blue-600 font-bold text-white p-2 border rounded shadow-md">
+              Back to CineMate
+            </button>
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+};
---
+++ src/pages/Search.js
@@ -0,0 +1,30 @@
+import { useSearchParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import { Card } from "../components";
+import { useTitle } from "../hooks/useTitle";
+
+export const Search = ({ api }) => {
+  const [searchParams] = useSearchParams();
+  const queryTerm = searchParams.get("q");
+  const { data: movies } = useFetch(api, queryTerm);
+  useTitle(`Search result for ${queryTerm}`);
+
+  return (
+    <main>
+      <section>
+        <p className=" text-3xl text-gray-700 dark:text-white">
+          {movies.length === 0
+            ? `No results found for'${queryTerm}'`
+            : `Result for'${queryTerm}'`}
+        </p>
+      </section>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
